69d63f1a2511d6fa70a911f4a3d472fea58a8c07
Ana Maria Guerrero-Campos <a.m.guerrero@tcu.edu>
2024-03-07 23:26:31 -0600

    Up to code along 18.

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -1,2 +1,20 @@
-package edu.tcu.cs.hogwartsartifactsonline.artifact;public class ArtifactController {
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import edu.tcu.cs.hogwartsartifactsonline.system.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArtifactController {
+    private final ArtifactService artifactService;
+
+    public ArtifactController(ArtifactService artifactService) {
+        this.artifactService = artifactService;
+    }
+
+    @GetMapping("/api/v1/artifacts/{artifactId}")
+    public Result findArtifactById(@PathVariable String artifactId) {
+        return null;
+    }
 }
